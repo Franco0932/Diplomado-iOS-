856f0c67d09bd7139da8782c0c5a4c003debac73
Franco Ruiz <francoruiz287@gmail.com>
2025-10-11 11:00:56 -0600

    App nueva

---
+++ Semana 9/Clase Sábado/AppModulo/AppModulo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  AppModulo
+//
+//  Created by Franco Ruiz on 11/10/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
