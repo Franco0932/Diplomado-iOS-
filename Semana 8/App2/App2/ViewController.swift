43487e22d15c74fb54ca4955385d63a6d345562a
Franco Ruiz <francoruiz287@gmail.com>
2025-10-11 11:03:06 -0600

    updates

---
+++ Semana 8/App2/App2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  App2
+//
+//  Created by Franco Ruiz on 04/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func myUnwindAction(unwindSegue:
+        UIStoryboardSegue){}
+
+}
+
---
+++ Semana 8/Ejercicio1ClaseViernes/Ejer5ClaseViernes/Ejer5ClaseViernes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejer5ClaseViernes
+//
+//  Created by Franco Ruiz on 10/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
