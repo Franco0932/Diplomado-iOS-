0d77ecbee51ae492756739f8f825889f468863b6
Franco Ruiz <francoruiz287@gmail.com>
2025-10-11 14:03:29 -0600

    actualizacion app modulo

---
+++ Semana 9/Clase Sábado/AppModulo/AppModulo/HomeViewController.swift
@@ -0,0 +1,45 @@
+//
+//  HomeViewController.swift
+//  AppModulo
+//
+//  Created by Franco Ruiz on 11/10/25.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var imageType: UISwitch!
+    @IBOutlet weak var captionSwitch: UISwitch!
+    @IBOutlet weak var captionTextSwitch: UISwitch!
+    @IBOutlet weak var captionTextFields: UITextField!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func picsButtonTapped(_ sender: Any){
+        
+    }
+    
+    @IBAction func logOutButtonTapped(_ sender: Any){
+        
+    }
+    
+    @IBAction func informationButtonTapped(_ sender: Any){
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
