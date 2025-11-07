271586895fc4110331f6376f4392d23a0493d2bf
Franco Ruiz <francoruiz287@gmail.com>
2025-11-07 16:18:05 -0600

    Semana 13 ejercicios

---
+++ Semana 13/Clase Viernes/Ejercicio2/Ejercicio2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio2
+//
+//  Created by Franco Ruiz on 07/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
